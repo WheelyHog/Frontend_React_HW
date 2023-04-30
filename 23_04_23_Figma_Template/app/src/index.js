65d9ae009a64a874a8dde24b25032f777c38238b
Sergii <wheelyhog@gmail.com>
2023-04-30 14:12:03 +0200

    update hw 23_04_23

--- 23_04_23_Figma_Template/app/src/index.js
+++ 23_04_23_Figma_Template/app/src/index.js
@@ -2,12 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
+
+const router = createHashRouter([
+  {
+    path: '/*',
+    element: <App />
+  }
+])
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <RouterProvider router={router}>
     <App />
-  </BrowserRouter>
+  </RouterProvider>
 );
 
